207cd8f90563d286c5c28d1efce582f7d5638e02
Youssef Riahi <youssef.riahi@mass.gov>
2018-11-27 12:35:59 -0500

    add cloud front

--- static-site/main.tf
+++ static-site/main.tf
@@ -37,3 +37,48 @@ resource "aws_acm_certificate" "certificate" {
   validation_method = "DNS"
 }
 
+
+// Cloudfront
+resource "aws_cloudfront_distribution" "sub_domain_distribution" {
+  origin {
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+    }
+
+    // S3 bucker url
+    domain_name = "${aws_s3_bucket.mds_static_site.website_endpoint}"
+    // This can be any name to identify this origin.
+    origin_id   = "${var.sub_domain_name}"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+    target_origin_id       = "${var.sub_domain_name}"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+}
