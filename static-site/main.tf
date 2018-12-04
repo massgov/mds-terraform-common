1b744bb05b639b49c8d40b7c12eeda90d00b8bee
Youssef Riahi <youssef.riahi@mass.gov>
2018-12-04 10:10:05 -0500

    rename identifier to "site"

--- static-site/main.tf
+++ static-site/main.tf
@@ -1,6 +1,6 @@
 // S3
-// static site bucket
-resource "aws_s3_bucket" "static_site" {
+// site bucket
+resource "aws_s3_bucket" "site" {
   // name bucket after sub-domain name
   bucket = "${var.sub_domain_name}"
   policy = <<POLICY
@@ -82,7 +82,7 @@ data "aws_lambda_function" "s3_headers" {
 resource "aws_cloudfront_distribution" "sub_domain_distribution" {
   origin {
     // S3 bucker url
-    domain_name = "${aws_s3_bucket.static_site.website_endpoint}"
+    domain_name = "${aws_s3_bucket.site.website_endpoint}"
 
     // identifies the origin with a name (can be any string of choice)
     origin_id   = "${var.sub_domain_name}"
@@ -154,5 +154,5 @@ resource "aws_cloudfront_distribution" "sub_domain_distribution" {
 // Cloudfront
 // create an identity to access origin
 resource "aws_cloudfront_origin_access_identity" "edge" {
-    comment = "Cloudfront ID for ${aws_s3_bucket.static_site.bucket}"
+    comment = "Cloudfront ID for ${aws_s3_bucket.site.bucket}"
 }
