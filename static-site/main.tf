5c03eec18ec439bd7a9e5e90c892774c88186e8e
Youssef Riahi <youssef.riahi@mass.gov>
2018-12-04 14:51:10 -0500

    create the CNAME record

--- static-site/main.tf
+++ static-site/main.tf
@@ -70,6 +70,16 @@ resource "aws_acm_certificate_validation" "default" {
 }
 
 
+// Route 53
+// Add CNAME entry for domain
+resource "aws_route53_record" "default" {
+  zone_id = "${var.zone_id}"
+  name    = "${var.domain_name}"
+  type    = "CNAME"
+  ttl     = "300"
+  records = ["${aws_cloudfront_distribution.domain_distribution.domain_name}"]
+}
+
 // Lambda
 // "AlwaysRequestIndexHTML"
 resource "aws_lambda_function" "origin_request_lambda" {
