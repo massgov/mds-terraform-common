83c5e5bdaf6a6a312d55fa799b63f2ddea2955cf
Youssef Riahi <youssef.riahi@mass.gov>
2018-12-04 14:32:04 -0500

    vars cleanup

--- static-site/variables.tf
+++ static-site/variables.tf
@@ -6,25 +6,27 @@ provider "aws" {
 // root domain name
 variable "zone_id" {
   type        = "string"
-  description = "The zone that domain will be added to"
+  description = "The zone that domain will be added to."
 }
 
 // new site domain name
 variable "domain_name" {
   type        = "string"
-  description = "The full domain name"
+  description = "The full domain name being added."
 }
 
+// get index.html lambda
 variable "always_get_index_html_lambda" {
   type    = "string"
   default = "AlwaysRequestIndexHTML"
-  description = "The lambda that always get index.html for static sites"
+  description = "The lambda that always get index.html for sites."
 }
 
+// add headers lambda
 variable "s3_edge_header_lambda" {
   type    = "string"
   default = "s3_edge_header"
-  description = "The lamda that adds s3 origin headers"
+  description = "The lamda that adds s3 origin headers."
 }
 
 variable "tags" {
