adf9359009c50dfd1d4af16b096fe1fadbb1543d
Rob Bayliss <rob@lastcallmedia.com>
2019-05-13 11:27:53 -0400

    Add Chamber Policy module

---
+++ chamberpolicy/README.md
@@ -0,0 +1,6 @@
+Chamber Policy
+==============
+
+This Terraform module renders IAM policies for reading or writing secrets stored with [Chamber](https://github.com/segmentio/chamber).
+
+It does not create any resources on its own - it only provides outputs for IAM policies that can be attached to IAM users, roles, or groups.
---
+++ chamberpolicy/main.tf
@@ -0,0 +1,61 @@
+
+
+data "aws_caller_identity" "identity" {}
+data "aws_region" "region" {}
+data "aws_kms_alias" "chamber_key" {
+  name = "${var.key_alias}"
+}
+locals {
+  region = "${coalesce(var.region, data.aws_region.region.current)}"
+  account_id = "${coalesce(var.account_id, data.aws_caller_identity.identity.account_id)}"
+  parameter_arn = "arn:aws:ssm:${local.region}:${local.account_id}:parameter/${var.namespace}"
+}
+
+data "aws_iam_policy_document" "read_policy" {
+  statement {
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:DescribeParameters"
+    ]
+    resources = ["${local.parameter_arn}"]
+  }
+  statement {
+    actions = ["kms:Decrypt"]
+    resources = ["${data.aws_kms_alias.chamber_key.arn}"]
+    condition {
+      test = "StringEquals"
+      values = ["${local.parameter_arn}"]
+      variable = "kms:EncryptionContext:PARAMETER_ARN"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "readwrite_policy" {
+  statement {
+    actions = [
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParameterHistory",
+      "ssm:GetParametersByPath",
+      "ssm:PutParameter",
+      "ssm:DeleteParameter",
+      "ssm:DeleteParameters"
+    ]
+  }
+  // Read (decrypt)
+  statement {
+    actions = ["kms:Decrypt"]
+    resources = ["${data.aws_kms_alias.chamber_key.arn}"]
+    condition {
+      test = "StringEquals"
+      values = ["${local.parameter_arn}"]
+      variable = "kms:EncryptionContext:PARAMETER_ARN"
+    }
+  }
+  // Write (encrypt)
+  statement {
+    actions = ["kms:Encrypt"]
+    resources = ["${data.aws_kms_alias.chamber_key.arn}"]
+  }
+}
