f92df7fed4b217ac2d9c9e6520e390fdbd5ce699
Rob Bayliss <rob@lastcallmedia.com>
2019-05-30 09:56:28 -0400

    Run automated update to 0.12 syntax

--- ecscluster/outputs.tf
+++ ecscluster/outputs.tf
@@ -1,9 +1,10 @@
 // The ID of the ASG.
 output "autoscaling_group_id" {
-  value = "${module.asg.autoscaling_group_id}"
+  value = module.asg.autoscaling_group_id
 }
 
 // ECS cluster name.
 output "ecs_cluster" {
-  value = "${aws_ecs_cluster.cluster.name}"
+  value = aws_ecs_cluster.cluster.name
 }
+
