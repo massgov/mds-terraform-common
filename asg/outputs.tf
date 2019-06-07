1398645361bcf6b0af747dd878df3ef412e076e2
Rob Bayliss <rob@lastcallmedia.com>
2019-06-07 14:42:50 -0400

    Remove unused outputs

--- asg/outputs.tf
+++ asg/outputs.tf
@@ -2,8 +2,3 @@
 output "autoscaling_group_id" {
   value = "${aws_autoscaling_group.default.id}"
 }
-
-// The developer policy
-output "developer_policies" {
-  value = ["${data.aws_iam_policy_document.developer.json}"]
-}
