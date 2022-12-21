28152a351e9c342e35aaca4aa48673f4c54df459
DmitriiTartynovLCM <dmitrii@lastcallmedia.com>
2022-12-22 00:02:39 +0100

    DP-25691: Let the lambda read the SSM parameter.

--- entrypoint-monitoring/main.tf
+++ entrypoint-monitoring/main.tf
@@ -1,5 +1,12 @@
 data "aws_region" "default" {}
 
+data "aws_caller_identity" "current" {}
+
+locals {
+  aws_region = data.aws_region.default.name
+  aws_account_id = data.aws_caller_identity.current.account_id
+}
+
 data "archive_file" "monitor_package" {
   type        = "zip"
   source_file = "${path.module}/lambda/dist/lambda.js"
@@ -37,6 +44,15 @@ data "aws_iam_policy_document" "monitor_inline_policy" {
       "arn:aws:apigateway:${data.aws_region.default.name}::/domainnames",
       "arn:aws:apigateway:${data.aws_region.default.name}::/domainnames/*/basepathmappings",
       "arn:aws:apigateway:${data.aws_region.default.name}::/apis",
+    ]
+  }
+
+  statement {
+    actions = [
+      "ssm:GetParameter",
+    ]
+    resources = [
+      "arn:aws:ssm:${local.aws_region}:${local.aws_account_id}:parameter${var.allowed_points_parameter}"
     ]
   }
 }
