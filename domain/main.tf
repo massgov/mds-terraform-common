3b3c9cd485a2299e6c39b2c772a9b97d21de4167
Jared <jared@lastcallmedia.com>
2023-03-14 16:54:27 -0400

    lol

--- domain/main.tf
+++ domain/main.tf
@@ -22,10 +22,18 @@ data "aws_route53_zone" "tld" {
 }
 
 resource "aws_route53_record" "validation" {
-  name    = aws_acm_certificate.default.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.default.domain_validation_options[0].resource_record_type
+  for_each = {
+    for dvo in aws_acm_certificate.default.domain_validation_options : dvo.domain_name => {
+      name = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  type    = each.value.type
   zone_id = data.aws_route53_zone.tld.zone_id
-  records = [aws_acm_certificate.default.domain_validation_options[0].resource_record_value]
+  records = [each.value.record]
   ttl     = "60"
 }
 
