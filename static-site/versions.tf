e10896e11a6a7115e54d60fd819f3a712748e066
Chris Shelton <chris@lastcallmedia.com>
2023-03-16 15:33:16 -0400

    fix static site domain in hashicorp/aws >= 3

--- static-site/versions.tf
+++ static-site/versions.tf
@@ -4,9 +4,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      # This may work with earlier versions, however the lowest version
-      # we have currently using it is 2.70.
-      version = ">= 2.70"
+      version = ">= 3"
     }
   }
 }
