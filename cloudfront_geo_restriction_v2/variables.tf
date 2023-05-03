774fa19665863e14a0b64f3124f98cb773bc4fa2
Jared <jared@lastcallmedia.com>
2023-05-03 14:48:11 -0400

    add fairly needless argument validation

--- cloudfront_geo_restriction_v2/variables.tf
+++ cloudfront_geo_restriction_v2/variables.tf
@@ -1,6 +1,10 @@
 variable "name_prefix" {
   type        = string
   description = "A name prefix to use for created resources."
+  validation {
+    condition = can(regex("^[a-zA-Z0-9-]*[a-zA-Z0-9]+$", var.name_prefix))
+    error_message = "Prefix should only contain alphanumeric characters and (optionally) dashes. It must not end in a dash."
+  }
 }
 
 variable "enable_cloudwatch_metrics" {
