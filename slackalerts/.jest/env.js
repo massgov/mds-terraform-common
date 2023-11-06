2264ac1d2910e94cc8136d42b025c2d80be4bd8c
Jared <jared@lastcallmedia.com>
2023-11-06 13:48:14 -0500

    make tests slightly more interesting

--- slackalerts/.jest/env.js
+++ slackalerts/.jest/env.js
@@ -1,3 +1,10 @@
 process.env.SLACK_TOKEN = '000000aa-aaaa-cccc-aaaa-bbbbbbbbbb00';
-process.env.TOPIC_MAP = JSON.stringify([]);
+process.env.TOPIC_MAP = JSON.stringify([
+  {
+    topic_arn: 'arn:aws:sns:us-east-1:123456789012:my-cool-topic',
+    icon_emoji: ':robot:',
+    channel: '#my-cool-alerts',
+    username: '@mds-ssr'
+  }
+]);
 process.env.DEFAULT_CHANNEL = '#alerts';
--- slackalerts/src/test/index.test.ts
+++ slackalerts/src/test/index.test.ts
@@ -21,37 +21,6 @@ beforeEach(() => {
 });
 
 describe('handler', () => {
-    const record: SNSEvent = {
-        "Records": [
-            {
-                "EventSource": "aws:sns",
-                "EventVersion": "1.0",
-                "EventSubscriptionArn": "arn:aws:sns:us-east-1:{{accountId}}:ExampleTopic",
-                "Sns": {
-                    "Type": "Notification",
-                    "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
-                    "TopicArn": "arn:aws:sns:us-east-1:123456789012:ExampleTopic",
-                    "Subject": "example subject",
-                    "Message": "example message",
-                    "Timestamp": "1970-01-01T00:00:00.000Z",
-                    "SignatureVersion": "1",
-                    "Signature": "EXAMPLE",
-                    "SigningCertUrl": "EXAMPLE",
-                    "UnsubscribeUrl": "EXAMPLE",
-                    "MessageAttributes": {
-                        "Test": {
-                            "Type": "String",
-                            "Value": "TestString"
-                        },
-                        "TestBinary": {
-                            "Type": "Binary",
-                            "Value": "TestBinary"
-                        }
-                    }
-                }
-            }
-        ]
-    };
 
     const context: Context = {
         callbackWaitsForEmptyEventLoop: false,
@@ -68,7 +37,60 @@ describe('handler', () => {
         succeed: () => {}
     };
 
-    it('correctly transforms the SNS event(s)', async (): Promise<void> => {
+    it('correctly transforms the SNS event', async (): Promise<void> => {
+        const record: SNSEvent = {
+            "Records": [
+                {
+                    "EventSource": "aws:sns",
+                    "EventVersion": "1.0",
+                    "EventSubscriptionArn": "arn:aws:sns:us-east-1:123456789012:ExampleTopic",
+                    "Sns": {
+                        "Type": "Notification",
+                        "MessageId": "e849714b-6b3f-4802-9575-bf4b894d7e92",
+                        "TopicArn": "arn:aws:sns:us-east-1:123456789012:my-cool-topic",
+                        "Subject": "subject",
+                        "Message": 'message',
+                        "Timestamp": "1970-01-01T00:00:00.000Z",
+                        "SignatureVersion": "1",
+                        "Signature": "EXAMPLE",
+                        "SigningCertUrl": "EXAMPLE",
+                        "UnsubscribeUrl": "EXAMPLE",
+                        "MessageAttributes": {}
+                    }
+                }
+            ]
+        };
+        await handler(
+            record,
+            context,
+            () => {}
+        );
+        expect(mockMessage).toMatchSnapshot();
+    });
+
+    it('correctly transforms the ClamAV SNS event', async (): Promise<void> => {
+        const record: SNSEvent = {
+            "Records": [
+                {
+                    "EventSource": "aws:sns",
+                    "EventVersion": "1.0",
+                    "EventSubscriptionArn": "arn:aws:sns:us-east-1:123456789012:ExampleTopic",
+                    "Sns": {
+                        "Type": "Notification",
+                        "MessageId": "95df01b4-ee98-5cb9-9903-4c221d41eb5e",
+                        "TopicArn": "arn:aws:sns:us-east-1:123456789012:massgov-clamav-scan-status",
+                        "Subject": "Clam AV test",
+                        "Message": JSON.stringify({ Foo: "Bar", Bar: "Baz" }),
+                        "Timestamp": "1970-01-01T00:00:00.000Z",
+                        "SignatureVersion": "1",
+                        "Signature": "EXAMPLE",
+                        "SigningCertUrl": "EXAMPLE",
+                        "UnsubscribeUrl": "EXAMPLE",
+                        "MessageAttributes": {}
+                    }
+                }
+            ]
+        };
         await handler(
             record,
             context,
