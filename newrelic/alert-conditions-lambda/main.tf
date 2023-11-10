fd074d57093ef33546ef67605060a3677d884ae0
Chris Shelton <chris@lastcallmedia.com>
2023-11-10 12:30:22 -0500

    add alert conditions for lambdas

---
+++ newrelic/alert-conditions-lambda/main.tf
@@ -0,0 +1,101 @@
+locals {
+  aws_accounts_quoted = join(", ", formatlist("'%s'", var.filter_aws_accounts))
+  aws_accounts_subquery = length(var.filter_aws_accounts) == 0 ? "" : "aws.accountId IN (${local.aws_accounts_quoted})"
+
+  function_names_quoted = join(", ", formatlist("'%s'", var.filter_function_names))
+  function_names_subquery = length(var.filter_function_names) == 0 ? "" : "`aws.lambda.FunctionName` IN (${local.function_names_quoted})"
+
+  filter_subqueries_and = join(" AND ", compact([local.aws_accounts_subquery, local.function_names_subquery]))
+
+  filter_subquery = length(local.filter_subqueries_and) == 0 ? "" : "WHERE (${local.filter_subqueries_and})"
+}
+
+resource "newrelic_nrql_alert_condition" "error_percent" {
+  count = (var.error_percent_threshold == null ? 0 : 1)
+
+  account_id = var.account_id
+  policy_id = var.alert_policy_id
+  type = "static"
+  name = "${var.name_prefix} - Error Percent"
+  enabled = true
+  violation_time_limit_seconds = 259200
+
+  nrql {
+    query = "SELECT average(`aws.lambda.Errors.byFunction`) * 100 FROM Metric ${local.filter_subquery} FACET `aws.lambda.FunctionName`"
+  }
+
+  critical {
+    operator = "above"
+    threshold = var.error_percent_threshold
+    threshold_duration = var.critical_threshold_duration
+    threshold_occurrences = "all"
+  }
+
+  fill_option = "last_value"
+  aggregation_window = var.aggregation_window
+  aggregation_method = "event_timer"
+  aggregation_timer = 60
+
+  open_violation_on_expiration = false
+  close_violations_on_expiration = false
+}
+
+resource "newrelic_nrql_alert_condition" "events_dropped" {
+  count = (var.events_dropped_threshold == null ? 0 : 1)
+
+  account_id = var.account_id
+  policy_id = var.alert_policy_id
+  type = "static"
+  name = "${var.name_prefix} - Events Dropped"
+  enabled = true
+  violation_time_limit_seconds = 259200
+
+  nrql {
+    query = "SELECT sum(`aws.lambda.AsyncEventsDropped`) FROM Metric ${local.filter_subquery} FACET `aws.lambda.FunctionName`"
+  }
+
+  critical {
+    operator = "above"
+    threshold = var.events_dropped_threshold
+    threshold_duration = var.critical_threshold_duration
+    threshold_occurrences = "all"
+  }
+
+  fill_option = "none"
+  aggregation_window = var.aggregation_window
+  aggregation_method = "event_timer"
+  aggregation_timer = 60
+
+  open_violation_on_expiration = false
+  close_violations_on_expiration = false
+}
+
+resource "newrelic_nrql_alert_condition" "duration" {
+  count = (var.duration_threshold == null ? 0 : 1)
+
+  account_id = var.account_id
+  policy_id = var.alert_policy_id
+  type = "static"
+  name = "${var.name_prefix} - Duration"
+  enabled = true
+  violation_time_limit_seconds = 259200
+
+  nrql {
+    query = "SELECT sum(`aws.lambda.Duration.byFunction`) FROM Metric ${local.filter_subquery} FACET `aws.lambda.FunctionName`"
+  }
+
+  critical {
+    operator = "above"
+    threshold = var.events_dropped_threshold
+    threshold_duration = var.critical_threshold_duration
+    threshold_occurrences = "all"
+  }
+
+  fill_option = "none"
+  aggregation_window = var.aggregation_window
+  aggregation_method = "event_timer"
+  aggregation_timer = 60
+
+  open_violation_on_expiration = false
+  close_violations_on_expiration = false
+}
---
+++ newrelic/alert-conditions-lambda/variables.tf
@@ -0,0 +1,75 @@
+variable "name_prefix" {
+  type        = string
+  description = "Name prefix for the alert condition"
+}
+
+variable "account_id" {
+  type        = string
+  description = "The account number for the New Relic account."
+}
+
+variable "alert_policy_id" {
+  type        = string
+  description = "The id of the New Relic alert policy."
+}
+
+variable "filter_aws_accounts" {
+  type        = list(string)
+  description = "List of AWS account ids to monitor."
+  default     = []
+}
+
+variable "filter_function_names" {
+  type        = list(string)
+  description = "List of Lambda Function names to monitor."
+  default     = []
+}
+
+variable "aggregation_window" {
+  type        = number
+  description = "See newrelic_nrql_alert_condition.aggregation_window."
+  default     = 3600
+}
+
+# This should be, at minimum, the frequency at which the lambda runs. If this
+# only runs once per hour, then it should be at least 3600.
+variable "critical_threshold_duration" {
+  type        = number
+  description = "See newrelic_nrql_alert_condition.critical.threshold_duration."
+  default     = 3600
+}
+
+variable "error_percent_threshold" {
+  description = "Maximum Error percentage (0 - 100%) allowed before triggering alert."
+  default     = 5
+
+  validation {
+    condition     = tonumber(var.error_percent_threshold) == var.error_percent_threshold || var.error_percent_threshold == null
+    error_message = "The error_percent_threshold value should either be a number or null (which would disable the error_percent alert)."
+  }
+
+  validation {
+    condition     = var.error_percent_threshold == null || (var.error_percent_threshold >= 0 && var.error_percent_threshold <= 100)
+    error_message = "The error_percent_threshold value should be between 0 and 100 (or null)."
+  }
+}
+
+variable "events_dropped_threshold" {
+  description = "Maximum number of dropped events allowed before triggering alert."
+  default     = 1
+
+  validation {
+    condition     = tonumber(var.events_dropped_threshold) == var.events_dropped_threshold || var.events_dropped_threshold == null
+    error_message = "The events_dropped_threshold value should either be a number or null (which would disable the events_dropped alert)."
+  }
+}
+
+variable "duration_threshold" {
+  description = "Duration (in milliseconds) allowed before triggering alert."
+  default     = 300000
+
+  validation {
+    condition     = tonumber(var.duration_threshold) == var.duration_threshold || var.duration_threshold == null
+    error_message = "The duration_threshold value should either be a number or null (which would disable the duration alert)."
+  }
+}
