293e747411db4b37be19d62ca4887fd68e5bbd5b
Jared <jared@lastcallmedia.com>
2023-11-30 16:32:52 -0500

    use ssr backup golden ami

--- golden-ami-lookup/main.tf
+++ golden-ami-lookup/main.tf
@@ -1,6 +1,6 @@
 # Default AMI to use when none is specified.
 data "aws_ami" "golden_ami" {
   most_recent = true
-  name_regex  = "^eotss-aws2-cis-lvm_"
-  owners      = ["704819628235"]
+  name_regex  = "^itd-mgt-golden-aws-linux2"
+  owners      = ["786775234217"]
 }
