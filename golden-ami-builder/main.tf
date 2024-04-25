1d578a657b755cc3ab28997efab5f30f37e10b03
Jared HM <jaredhm@users.noreply.github.com>
2024-04-25 17:41:07 -0400

    DP-31742: prevent reserved tags from being applied to image builder resources (#180)

--- golden-ami-builder/main.tf
+++ golden-ami-builder/main.tf
@@ -164,8 +164,12 @@ resource "aws_imagebuilder_infrastructure_configuration" "golden_ami" {
     }
   }
 
-  resource_tags = var.tags
-  tags          = var.tags
+  resource_tags = {
+    # 'CreatedBy' and 'Ec2ImageBuilderArn' are reserved tags for instances created by
+    # Image Builder. Trying to provide either will kill deployments
+    for k, v in var.tags : k => v if !contains(["createdby", "ec2imagebuilderarn"], lower(k))
+  }
+  tags = var.tags
 }
 
 resource "aws_imagebuilder_component" "download_and_install_cortex_xdr" {
