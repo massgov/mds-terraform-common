b00185a45843b6fc0180468450ee265a2e14b0be
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2024-05-22 07:51:29 -0500

    dp-31821: update versions

--- ecs_fargate/versions.tf
+++ ecs_fargate/versions.tf
@@ -1,8 +1,16 @@
+
 terraform {
+  required_version = ">= 0.13"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.48.0"
+      # Needed for lambda module
+      version = ">= 4.8.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0"
     }
   }
 }
