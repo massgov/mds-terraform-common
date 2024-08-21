49ed29fb5aa0320c294878d6e78e29f6892fb112
Dan Houle <daniel.houle@mass.gov>
2024-08-21 11:19:27 -0700

    import jump box and session monitor modules

---
+++ jump-box/variables.tf
@@ -0,0 +1,46 @@
+variable "vpc_id" {
+  type        = string
+  description = "The VPC to launch the instance in."
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The VPC subnet to launch the instance in."
+}
+
+variable "ami" {
+  type        = string
+  description = "The ami to use for the jump box. If the SSM Agent is not already installed on the ami, it should be installed through the user_data variable."
+}
+
+variable "name" {
+  type        = string
+  description = "Name to use for the instance."
+}
+
+variable "instance_type" {
+  type        = string
+  description = "The instance type to use for the instance."
+  default     = "t4g.nano"
+}
+
+variable "volume_size" {
+  type        = number
+  description = "Size of the root volume in gibibytes."
+  default     = null
+  nullable    = true
+}
+
+variable "volume_type" {
+  type        = string
+  description = "Volume type for the root volume."
+  default     = null
+  nullable    = true
+}
+
+variable "user_data" {
+  type        = string
+  description = "User data to use when creating the instance."
+  default     = null
+}
+
