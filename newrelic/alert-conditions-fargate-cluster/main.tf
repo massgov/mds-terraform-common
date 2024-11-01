de3860c2caad097f0bdb26dff73732608580e75e
Jared <jared@lastcallmedia.com>
2024-11-01 11:38:01 -0400

    also add this

---
+++ newrelic/alert-conditions-fargate-cluster/main.tf
@@ -0,0 +1,92 @@
+locals {
+  aws_accounts_quoted   = join(", ", formatlist("'%s'", var.filter_aws_accounts))
+  aws_accounts_subquery = length(var.filter_aws_accounts) == 0 ? "" : "aws.accountId IN (${local.aws_accounts_quoted})"
+
+  cluster_names_quoted = join(", ", formatlist("'%s'", var.filter_cluster_names))
+  cluster_names_subquery = length(var.filter_cluster_names) == 0 ? "" : (
+    "aws.ecs.containerinsights.ClusterName IN (${local.cluster_names_quoted})"
+  )
+
+  filter_subqueries_and = join(" AND ", compact([local.aws_accounts_subquery, local.cluster_names_subquery]))
+
+  filter_subquery = length(local.filter_subqueries_and) == 0 ? "" : "WHERE (${local.filter_subqueries_and})"
+}
+
+module "cpu" {
+  source = "../nrql-alert"
+
+  account_id = var.account_id
+  policy_id  = var.alert_policy_id
+  name = format(
+    "%s - CPU utilization over %s%% for at least %d seconds",
+    var.name_prefix,
+    replace(format("%f", var.cpu_threshold), "/\\.0+$/", ""),
+    var.critical_threshold_duration
+  )
+
+  nrql_query = <<EOF
+    SELECT 100 * average(`aws.ecs.containerinsights.CpuUtilized`)/average(`aws.ecs.containerinsights.CpuReserved`)
+    FROM Metric
+    ${local.filter_subquery}
+    FACET aws.ecs.containerinsights.ClusterName
+  EOF
+
+  critical_threshold          = var.cpu_threshold
+  critical_threshold_duration = var.critical_threshold_duration
+  aggregation_window          = var.aggregation_window
+  aggregation_method          = "event_flow"
+  aggregation_delay           = 120
+  tags                        = var.tags
+}
+
+module "memory" {
+  source = "../nrql-alert"
+
+  account_id = var.account_id
+  policy_id  = var.alert_policy_id
+  name = format(
+    "%s - Memory usage over %s%% for at least %d seconds",
+    var.name_prefix,
+    replace(format("%f", var.memory_threshold), "/\\.0+$/", ""),
+    var.critical_threshold_duration
+  )
+
+  nrql_query = <<EOF
+    SELECT 100 * average(`aws.ecs.containerinsights.MemoryUtilized`) / average(`aws.ecs.containerinsights.MemoryReserved`)
+    FROM Metric
+    ${local.filter_subquery}
+    FACET aws.ecs.containerinsights.ClusterName
+  EOF
+  critical_threshold          = var.memory_threshold
+  critical_threshold_duration = var.critical_threshold_duration
+  aggregation_window          = var.aggregation_window
+  aggregation_method          = "event_flow"
+  aggregation_delay           = 120
+  tags                        = var.tags
+}
+
+module "task_count" {
+  source = "../nrql-alert"
+
+  account_id = var.account_id
+  policy_id  = var.alert_policy_id
+  name = format(
+    "%s - Task count under %d for at least %d seconds",
+    var.name_prefix,
+    replace(format("%f", var.task_count_threshold), "/\\.0+$/", ""),
+    var.critical_threshold_duration
+  )
+
+  nrql_query = <<EOF
+    SELECT average(`aws.ecs.containerinsights.TaskCount`)
+    FROM Metric
+    ${local.filter_subquery}
+    FACET aws.ecs.containerinsights.ClusterName
+  EOF
+  critical_threshold          = var.task_count_threshold
+  critical_threshold_duration = var.task_count_threshold_duration
+  aggregation_window          = var.aggregation_window
+  aggregation_method          = "event_flow"
+  aggregation_delay           = 120
+  tags                        = var.tags
+}
---
+++ newrelic/alert-conditions-fargate-cluster/variables.tf
@@ -0,0 +1,68 @@
+variable "name_prefix" {
+  type        = string
+  description = "Name prefix for the alert condition"
+}
+
+variable "account_id" {
+  type        = string
+  description = "The account number for the New Relic account."
+}
+
+variable "alert_policy_id" {
+  type        = string
+  description = "The id of the New Relic alert policy."
+}
+
+variable "filter_aws_accounts" {
+  type        = list(string)
+  description = "List of AWS account ids to monitor."
+  default     = []
+}
+
+variable "filter_cluster_names" {
+  type        = list(string)
+  description = "List of ECS cluster names to monitor. Note that the clusters being monitored must have Container Insights enabled."
+  default     = []
+}
+
+variable "aggregation_window" {
+  type        = number
+  description = "See newrelic_nrql_alert_condition.aggregation_window."
+  default     = 60
+}
+
+variable "critical_threshold_duration" {
+  type        = number
+  description = "See newrelic_nrql_alert_condition.critical.threshold_duration."
+  default     = 300
+}
+
+variable "cpu_threshold" {
+  type        = number
+  description = "Maximum CPU percentage allowed before triggering alert."
+  default     = 90
+}
+
+variable "memory_threshold" {
+  type        = number
+  description = "Maximum memory percentage allowed before triggering alert."
+  default     = 90
+}
+
+variable "task_count_threshold" {
+  type        = number
+  description = "Minimum number of tasks in cluster before triggering alert"
+  default     = 3
+}
+
+variable "task_count_threshold_duration" {
+  type        = number
+  description = "See newrelic_nrql_alert_condition.critical.threshold_duration."
+  default     = 600
+}
+
+variable "tags" {
+  type        = map(any)
+  description = "Tags to apply to the alert conditions. Tag values can either be a single string or a list of strings."
+  default     = {}
+}
