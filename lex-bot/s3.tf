2f7290d7df7b7054881556fbc78c42977a4b9695
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2024-12-06 14:04:46 -0600

    DP-36024: initial chatbot code push

--- lex-bot/s3.tf
+++ lex-bot/s3.tf
@@ -11,11 +11,31 @@ resource "aws_s3_bucket" "source_bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "default" {
+resource "aws_s3_bucket_public_access_block" "source_bucket" {
   for_each = toset(var.environments)
   bucket                  = aws_s3_bucket.source_bucket[each.key].id
   block_public_acls       = true
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "source_bucket" {
+
+  for_each = toset(var.environments)
+  bucket                  = aws_s3_bucket.source_bucket[each.key].id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "source_bucket" {
+
+  for_each = toset(var.environments)
+  bucket                  = aws_s3_bucket.source_bucket[each.key].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
