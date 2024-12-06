f4489cc73c7f52712cd7bb388425b528ad4fb826
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2024-12-06 14:05:01 -0600

    DP-36024: initial chatbot code push

--- lex-bot/s3.tf
+++ lex-bot/s3.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "source_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "source_bucket" {
-  for_each = toset(var.environments)
+  for_each                = toset(var.environments)
   bucket                  = aws_s3_bucket.source_bucket[each.key].id
   block_public_acls       = true
   block_public_policy     = true
@@ -21,17 +21,15 @@ resource "aws_s3_bucket_public_access_block" "source_bucket" {
 }
 
 resource "aws_s3_bucket_versioning" "source_bucket" {
-
   for_each = toset(var.environments)
-  bucket                  = aws_s3_bucket.source_bucket[each.key].id
+  bucket   = aws_s3_bucket.source_bucket[each.key].id
   versioning_configuration {
     status = "Enabled"
   }
 }
 resource "aws_s3_bucket_server_side_encryption_configuration" "source_bucket" {
-
   for_each = toset(var.environments)
-  bucket                  = aws_s3_bucket.source_bucket[each.key].id
+  bucket   = aws_s3_bucket.source_bucket[each.key].id
 
   rule {
     apply_server_side_encryption_by_default {
