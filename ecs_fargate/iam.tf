4850cfcee4ca42335d5e1daab8175764c1e7f5ee
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2025-03-07 12:18:03 -0600

    DP-36917: update to support schedule task only

--- ecs_fargate/iam.tf
+++ ecs_fargate/iam.tf
@@ -1,4 +1,6 @@
 data "aws_iam_policy_document" "assume_role" {
+  count              = var.ecs_task_def != null ? 1 : 0
+
   statement {
     effect = "Allow"
 
@@ -11,6 +13,7 @@ data "aws_iam_policy_document" "assume_role" {
   }
 }
 data "aws_iam_policy_document" "ecs_events_run_task_with_any_role" {
+  count              = var.ecs_task_def != null ? 1 : 0
   statement {
     effect    = "Allow"
     actions   = ["iam:PassRole"]
@@ -26,12 +29,12 @@ data "aws_iam_policy_document" "ecs_events_run_task_with_any_role" {
 resource "aws_iam_role" "ecs_schedule_role" {
   count = var.ecs_task_only && var.ecs_task_schedule != "" ? 1 : 0
   name               = join("-", [var.ecs_cluster_name, var.ecs_task_name, "schedule", "role"])
-  assume_role_policy = data.aws_iam_policy_document.ecs_events_run_task_with_any_role.json
+  assume_role_policy = data.aws_iam_policy_document.assume_role[0].json
 }
 resource "aws_iam_role_policy" "ecs_events_run_task_with_any_role" {
   count = var.ecs_task_only && var.ecs_task_schedule != "" ? 1 : 0
 
   name   = "ecs_events_run_task_with_any_role"
   role   = aws_iam_role.ecs_schedule_role[0].name
-  policy = data.aws_iam_policy_document.ecs_events_run_task_with_any_role.json
+  policy = data.aws_iam_policy_document.ecs_events_run_task_with_any_role[0].json
 }
