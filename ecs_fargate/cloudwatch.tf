5c7cbe9024e38fe0c39f9f7b1019bc6021134d07
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2025-03-07 13:08:12 -0600

    DP-36917: update to support schedule task only

--- ecs_fargate/cloudwatch.tf
+++ ecs_fargate/cloudwatch.tf
@@ -1,6 +1,6 @@
 locals {
   log_groups = var.ecs_task_def != null ? {
-    for k, v in var.ecs_task_def.containers : v.container_name => v
+    for k, v in var.ecs_task_def.containers : v.container_name => v.log_group_name
     if try(v.log_group_name, null) != null
   } : {}
 }
