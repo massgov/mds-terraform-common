4a4b25c830f51cbb7e265d58ff501908f388a51e
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2025-03-07 13:06:02 -0600

    DP-36917: update to support schedule task only

--- ecs_fargate/cloudwatch.tf
+++ ecs_fargate/cloudwatch.tf
@@ -1,6 +1,6 @@
 locals {
   log_groups = var.ecs_task_def != null ? {
-    for k, v in var.ecs_task_def.containers : "${v.container_name}" => v
+    for k, v in var.ecs_task_def.containers : v.container_name => v
     if try(v.log_group_name, null) != null
   } : {}
 }
@@ -11,13 +11,13 @@ output "log_groups" {
 resource "aws_cloudwatch_log_group" "main" {
   for_each = local.log_groups
 
-  name              = join("/", ["ecs", terraform.workspace, var.ecs_cluster_name, var.ecs_service_name, lookup(each.value, "container_name")])
+  name              = each.key
   retention_in_days = var.log_retention_days
   kms_key_id        = var.cw_kms_key
   tags = merge(
     var.tags,
     {
-      "Name" = join("/", ["ecs", terraform.workspace, var.ecs_cluster_name, var.ecs_service_name, lookup(each.value, "container_name")])
+      "Name" = each.key
     },
   )
 }
