66773d05314487394e489c7178f8d00f48498b78
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2025-06-25 09:29:55 -0500

    DP-33807: add support to pass a manifest file to upload source doc files to s3

--- tagging/s3.tf
+++ tagging/s3.tf
@@ -11,7 +11,8 @@ resource "aws_s3_object" "files" {
 
   bucket = data.aws_s3_bucket.docs.id
   key    = "documentation/${var.repo}/${replace(replace(each.value, "../", ""), "./", "")}"
-  source = "${local.relative_path}${replace(replace(each.value, "../", ""), "./", "")}" # Update with the path to your files
+  source = "${local.relative_path}${replace(replace(each.value, "../", ""), "./", "")}"
+  etag   = filemd5("${local.relative_path}${replace(replace(each.value, "../", ""), "./", "")}")  # Use MD5 hash to detect changes
 }
 
 resource "aws_s3_object" "category" {
