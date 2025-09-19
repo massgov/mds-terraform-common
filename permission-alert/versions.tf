7bc9a6fa829fc8b3481b146744c672cc3c3174e0
Jimmy-Moran <49790040+JimmyIL@users.noreply.github.com>
2025-09-19 15:39:54 -0500

    Update versions.tf

--- permission-alert/versions.tf
+++ permission-alert/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
+      version = ">= 5.87.0, < 6.0.0"
     }
     archive = {
       source  = "hashicorp/archive"
@@ -10,3 +10,4 @@ terraform {
     }
   }
 }
+
