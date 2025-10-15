7bfda1e319f601c7d2aeb62d8efbc3dca84b610e
Jared <jared@lastcallmedia.com>
2025-10-15 16:32:49 -0400

    terraform-docs

--- proto-instance/variables.tf
+++ proto-instance/variables.tf
@@ -12,7 +12,10 @@ variable "cpu_options" {
     core_count       = number
     threads_per_core = number
   })
-  description = "CPU options to pass to instance template. See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/cpu-options-supported-instances-values.html"
+  description = <<EOF
+    CPU options to pass to instance template.
+    See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/cpu-options-supported-instances-values.html
+  EOF
   default = {
     core_count       = 1
     threads_per_core = 2
@@ -21,7 +24,10 @@ variable "cpu_options" {
 
 variable "instance_type" {
   type        = string
-  description = "EC2 instance type. See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/cpu-options-supported-instances-values.html"
+  description = <<EOF
+    EC2 instance type.
+    See https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/cpu-options-supported-instances-values.html
+  EOF
   default     = "m4.large"
 }
 
@@ -33,8 +39,7 @@ variable "subnet_id" {
 variable "security_group_ids" {
   type        = list(string)
   description = <<EOF
-    List of security group IDs to attach to the instance. If no list is provided, a bespoke Security
-    Group will automatically be created with all egress permitted and no ingress permitted.
+    List of security group IDs to attach to the instance. If no list is provided, a bespoke Security Group will automatically be created with all egress permitted and no ingress permitted.
   EOF
   default     = null
 }
@@ -42,9 +47,8 @@ variable "security_group_ids" {
 variable "management_lambda_schedule_expression" {
   type = string
   description = <<EOF
-    Schedule expression to pass to EventBridge Scheduler for management Lambda invocation. If null, the
-    Lambda will not be scheduled to automatically run. See
-    https://docs.aws.amazon.com/scheduler/latest/UserGuide/schedule-types.html for more information
+    Schedule expression to pass to EventBridge Scheduler for management Lambda invocation. If null, the Lambda will not be scheduled to automatically run.
+    See https://docs.aws.amazon.com/scheduler/latest/UserGuide/schedule-types.html for more information
   EOF
   default = "rate(14 days)"
 }
@@ -55,8 +59,7 @@ variable "ami_search_filters" {
     values = list(string)
   }))
   description = <<EOF
-    List of filters to be applied to AMI search. Note that changing the OS distribution may
-    have unintended consequences, as user data scripts have only been tested in RHEL 10.x.
+    List of filters to be applied to AMI search. Note that changing the OS distribution may have unintended consequences, as user data scripts have only been tested in RHEL 10.x.
   EOF
   default = [
     {
@@ -73,9 +76,8 @@ variable "ami_search_filters" {
 variable "tag_specifications" {
   type        = map(map(string))
   description = <<EOF
-    Tags to be passed to instance launch template, in addition to provider-level default tags,
-    which will automatically be merged in. See https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_LaunchTemplateTagSpecificationRequest.html
-    for a list of valid resource types.
+    Tags to be passed to instance launch template, in addition to provider-level default tags, which will automatically be merged in.
+    See https://docs.aws.amazon.com/AWSEC2/latest/APIReference/API_LaunchTemplateTagSpecificationRequest.html for a list of valid resource types.
   EOF
   default = {
     "instance" = {
@@ -120,8 +122,7 @@ variable "user_volume_iops" {
 variable "instance_role_name" {
   type        = string
   description = <<EOF
-    Friendly name of IAM role to attach to instance profile. Defaults to creating bespoke role
-    with AmazonSSMManagedInstanceCore managed policy attached.
+    Friendly name of IAM role to attach to instance profile. Defaults to creating bespoke role with AmazonSSMManagedInstanceCore managed policy attached.
   EOF
   default     = null
 }
@@ -133,8 +134,7 @@ variable "additional_clountinit_config_parts" {
     content      = string
   }))
   description = <<EOF
-    Cloudinit configuration files to include in user data. See
-    https://cloudinit.readthedocs.io/en/latest/explanation/format.html"
+    Cloudinit configuration files to include in user data. See https://cloudinit.readthedocs.io/en/latest/explanation/format.html"
   EOF
   default     = []
 }
