cb35e11bda04cb310a55d8bc589305cb06101d5c
Justin Bahr <justinbahr@Justins-MacBook-Pro.local>
2026-03-19 12:38:31 -0500

    DP-43051: update new relic account int to custom enable aws services.

--- newrelic/account-intergrate/locals.tf
+++ newrelic/account-intergrate/locals.tf
@@ -3,8 +3,8 @@ locals {
     for name, user_cfg in var.enabled_integrations : name => {
       metrics_polling_interval = coalesce(lookup(user_cfg, "metrics_polling_interval", null), var.default_metrics_polling_interval)
       aws_regions              = coalesce(lookup(user_cfg, "aws_regions", null), var.default_aws_regions)
-      # tag_key = coalesce(lookup(user_cfg, "tag_key", null), var.default_tag_key)
-      # tag_value = coalesce(lookup(user_cfg, "tag_value", null), var.default_tag_value)
+      tag_key                  = var.default_tag_key
+      tag_value                = var.default_tag_value
       fetch_tags               = coalesce(lookup(user_cfg, "fetch_tags", null), var.default_fetch_tags)
       fetch_extended_inventory = coalesce(lookup(user_cfg, "fetch_extended_inventory", null), var.default_fetch_extended_inventory)
     }
