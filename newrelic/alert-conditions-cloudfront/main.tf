4a6bc3dffc167131631f56a42812c5360eb6880e
Jimmy-Moran <49790040+JimmyIL@users.noreply.github.com>
2026-05-04 18:13:21 -0500

    Update main.tf

--- newrelic/alert-conditions-cloudfront/main.tf
+++ newrelic/alert-conditions-cloudfront/main.tf
@@ -1,24 +1,33 @@
 locals {
-  aws_accounts_quoted   = join(", ", formatlist("'%s'", var.filter_aws_accounts))
-  aws_accounts_subquery = length(var.filter_aws_accounts) == 0 ? "" : "aws.accountId IN (${local.aws_accounts_quoted})"
+  aws_accounts_quoted = join(", ", [
+    for account in var.filter_aws_accounts : "'${replace(account, "'", "\\'")}'"
+  ])
 
-  distribution_names_quoted   = join(", ", formatlist("'%s'", var.filter_distribution_names))
-  distribution_names_subquery = length(var.filter_distribution_names) == 0 ? "" : "entity.name IN (${local.distribution_names_quoted})"
+  aws_accounts_subquery = length(var.filter_aws_accounts) == 0 ? "" : "`aws.accountId` IN (${local.aws_accounts_quoted})"
 
-  filter_subqueries_and = join(" AND ", compact([local.aws_accounts_subquery, local.distribution_names_subquery]))
+  distribution_names_quoted = join(", ", [
+    for name in var.filter_distribution_names : "'${replace(name, "'", "\\'")}'"
+  ])
 
-  filter_subquery = length(local.filter_subqueries_and) == 0 ? "" : "WHERE (${local.filter_subqueries_and})"
+  distribution_names_subquery = length(var.filter_distribution_names) == 0 ? "" : "`entity.name` IN (${local.distribution_names_quoted})"
+
+  filter_subqueries_and = join(" AND ", compact([
+    local.aws_accounts_subquery,
+    local.distribution_names_subquery
+  ]))
+
+  filter_subquery = local.filter_subqueries_and == "" ? "" : "WHERE (${local.filter_subqueries_and})"
 
   error_rate_nql_query = var.custom_error_rate_nql_query != "" ? var.custom_error_rate_nql_query : <<-NRQL
-    SELECT average(aws.cloudfront.TotalErrorRate) FROM Metric ${local.filter_subquery} FACET entity.name
+    SELECT average(`aws.cloudfront.TotalErrorRate`) FROM Metric ${local.filter_subquery} FACET `entity.name`
   NRQL
 
   important_error_rate_nql_query = <<-NRQL
-   SELECT average(aws.cloudfront.TotalErrorRate) - average(aws.cloudfront.4xxErrorRate) FROM Metric ${local.filter_subquery} FACET entity.name
+    SELECT average(`aws.cloudfront.TotalErrorRate`) - average(`aws.cloudfront.4xxErrorRate`) FROM Metric ${local.filter_subquery} FACET `entity.name`
   NRQL
 
   throughput_nql_query = var.custom_throughput_nql_query != "" ? var.custom_throughput_nql_query : <<-NRQL
-   SELECT average(aws.cloudfront.Requests) FROM Metric ${local.filter_subquery} FACET entity.name
+    SELECT average(`aws.cloudfront.Requests`) FROM Metric ${local.filter_subquery} FACET `entity.name`
   NRQL
 }
 
@@ -27,12 +36,14 @@ module "error_rate" {
 
   account_id = var.account_id
   policy_id  = var.alert_policy_id
+
   name = format(
     "%s - Error rate over %s%% for at least %d seconds",
     var.name_prefix,
     replace(format("%f", var.error_rate_threshold), "/\\.0+$/", ""),
     var.critical_threshold_duration
   )
+
   # if var.include_4xx_errors is false (default is true), we exclude 4xx errors in the error rate calculation
   nrql_query                  = var.include_4xx_errors ? local.error_rate_nql_query : local.important_error_rate_nql_query
   critical_threshold          = var.error_rate_threshold
@@ -45,11 +56,13 @@ module "error_rate" {
 
 module "throughput" {
   source = "../nrql-alert"
-  count  = (var.throughput_enabled ? 1 : 0)
+  count  = var.throughput_enabled ? 1 : 0
 
   account_id = var.account_id
   policy_id  = var.alert_policy_id
-  name = format("%s - Less than %d requests per %d seconds for over %d seconds",
+
+  name = format(
+    "%s - Less than %d requests per %d seconds for over %d seconds",
     var.name_prefix,
     var.throughput_threshold,
     var.aggregation_window,
