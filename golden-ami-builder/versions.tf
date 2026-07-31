95c787a5cfe6a2bf710c93ae35676294efbf2065
JimmyIL <49790040+JimmyIL@users.noreply.github.com>
2026-07-31 14:28:26 -0500

    golden-ami-builder/lookup version.tf

--- golden-ami-builder/versions.tf
+++ golden-ami-builder/versions.tf
@@ -1,12 +1,12 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.5.0"
   required_providers {
     aws = {
-      version = ">= 4.58"
+      version = ">= 5.26.0"
     }
     semvers = {
       source  = "anapsix/semvers"
-      version = ">= 0.7.1"
+      version = ">= 0.8.0"
     }
   }
 }
