e4fbe533bfa9d6f834f428499e48b403ce2db7df
JimmyIL <49790040+JimmyIL@users.noreply.github.com>
2026-07-31 11:21:01 -0500

    Revert "Updated ami-block-device-reader"
    This reverts commit a8eb47f566b4790786dc76b5e46229e51dcc8c3d.

--- ami-block-device-reader/versions.tf
+++ ami-block-device-reader/versions.tf
@@ -1,9 +1,11 @@
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 0.13"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.26.0"
+      source = "hashicorp/aws"
+      # This may work with earlier versions, however the lowest version
+      # we have currently using it is 2.70.
+      version = ">= 2.70"
     }
   }
 }
