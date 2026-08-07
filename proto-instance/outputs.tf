b70ee04a75bb48b5804a16658f26c075ba997db2
Jared MacFarlane <jared.macfarlane@mass.gov>
2026-08-07 12:52:21 -0400

    proto_id is useless

--- proto-instance/outputs.tf
+++ proto-instance/outputs.tf
@@ -2,6 +2,14 @@ output "launch_template_arn" {
   value = aws_launch_template.default.arn
 }
 
-output "proto_id" {
-  value = local.proto_id
+output "instance_id" {
+  value = aws_instance.default.id
+}
+
+output "s3fs_bucket_id" {
+  value = aws_s3_bucket.s3fs.id
+}
+
+output "s3fs_file_system_id" {
+  value = aws_s3files_file_system.s3fs.id
 }
