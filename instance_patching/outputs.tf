233e25e5d56dffe36c7a2fecd260cf8ee0c3cdab
Jimmy-Moran <49790040+JimmyIL@users.noreply.github.com>
2026-09-01 12:42:44 -0500

    DP-48592 : Update instance patching module (#312)
    -  Replaced hard-coded values with random string generation.
    - Replaced instance data lookups supplied as target with tag matching
    for targets.
    
    (below change finds every ssm managed instance/node that matches the
    passed environment tag value)
    targets {
      key    = "tag:environment"
      values = sort(tolist(var.patch_environments))
    }

--- instance_patching/outputs.tf
+++ instance_patching/outputs.tf
@@ -1,7 +1,18 @@
-output "instances_selected_for_patching" {
-  value = local.patch_instance_ids
+output "patch_target_configuration" {
+  value = {
+    target_key    = "tag:environment"
+    target_values = sort(tolist(var.patch_environments))
+  }
 }
 
-output "ecs_instances_excluded" {
-  value = sort(tolist(local.excluded_container_nodes))
+output "patch_association_id" {
+  value = aws_ssm_association.extended_patching.association_id
+}
+
+output "patch_association_arn" {
+  value = aws_ssm_association.extended_patching.arn
+}
+
+output "container_host_guard_document" {
+  value = aws_ssm_document.container_host_guard.name
 }
